c5591d8941128ee0255174a05c76de246edb8ab4
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-09-16 19:51:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
+API_ID = int(getenv("API_ID", "22475741"))
+API_HASH = getenv("API_HASH", "1a217be71a0225e0a678af286c211f8a")
+BOT_TOKEN = getenv("BOT_TOKEN", "7544718340:AAFTOwtcmKhA44PeL572OLTgtFxlDfUri4k")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6562642609").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://navedmohammad053:Aslam@8005@telegram.9xlun.mongodb.net/?retryWrites=true&w=majority&appName=Telegram")
+LOG_GROUP = getenv("LOG_GROUP", "2409732732")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
