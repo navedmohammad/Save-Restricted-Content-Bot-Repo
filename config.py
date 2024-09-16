a2a7ae3893b9ec48bea633d9b03677fac74adf13
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-09-16 19:53:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7544718340:AAFTOwtcmKhA44PeL572OLTgtFxlDfUri4k"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6562642609").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://navedmohammad053:Aslam@8005@telegram.9xlun.mongodb.net/?retryWrites=true&w=majority&appName=Telegram")
 LOG_GROUP = getenv("LOG_GROUP", "4576435324")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "2409732732"))
